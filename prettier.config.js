c5f3f8e185a9d79c8e2dd74cbdbc1b74c2ec8f91
Abramov Arthur <rifttech.arthur@gmail.com>
2019-11-04 20:29:48 +0300

    add prettier

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+    trailingComma: "all",
+    tabWidth: 4,
+    semi: false,
+    singleQuote: false,
+    bracketSpacing: true,
+    arrowParens: "avoid",
+    endOfLine: "lf"
+  };
